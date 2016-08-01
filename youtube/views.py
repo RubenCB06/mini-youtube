c8281fec1947c7b6661e9fa59457538653201596
rubencb <rnacho_22208@live.com.mx>
2016-07-31 21:36:05 -0500

    inicio del primer template

--- youtube/views.py
+++ youtube/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+class PrincipalView(View):
+	def get(self,request):
+		template_name = 'principal.html'
+		return render(request,template_name)
