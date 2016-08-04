585ccf35cbe61218dc16c8b4b9e13deba592e212
rubencb <rnacho_22208@live.com.mx>
2016-08-04 16:01:24 -0500

    pagina principal youtube

--- youtube/views.py
+++ youtube/views.py
@@ -4,4 +4,5 @@ from django.views.generic import View
 class PrincipalView(View):
 	def get(self,request):
 		template_name = 'principal.html'
+		#template_name = 'reproductor.html'
 		return render(request,template_name)
