fafcce4947638257c7305e9ab8d446c219a8fb5d
rubencb <rnacho_22208@live.com.mx>
2016-08-05 15:39:54 -0500

    Instalacion pillow para hacer migracion

--- youtube/models.py
+++ youtube/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+#from django.core.utlresolvers import reverse
 
-# Create your models here.
+class Video(models.Model):
+	titulo = models.CharField(max_length=200)
+	photo = models.ImageField(upload_to='youtube/images/')
+	url = models.CharField(max_length = 200)
+	autor = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=500)
+
+	def __str__(self):
+			return self.titulo
+
+	#def get_absolute_url(self):
+	#	return reverse('detalle',args=[self.slug])
--- youtube/views.py
+++ youtube/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Video
 
 class PrincipalView(View):
 	def get(self,request):
 		template_name = 'principal.html'
+		videos = Video.objects.all().order_by('titulo')
+		context = {'videos':videos,}
 		#template_name = 'reproductor.html'
-		return render(request,template_name)
+		return render(request,template_name,context)
